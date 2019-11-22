27cbf0420cfb93a300c60ab8f26e5309a7c5696c
Marie Marie <Mariedefre87@gmail.com>
2019-11-22 11:32:47 +0100

    first commit

---
+++ src/Controller/WildController.php
@@ -0,0 +1,23 @@
+<?php
+
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WildController extends AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/wild", name="wild_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
